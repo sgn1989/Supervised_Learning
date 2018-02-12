4f2cd329e43113121d69d12319fb4840c09e3eb0
sgn1989 <sgn1989@gmail.com>
2018-02-12 15:02:01 -0800

    Fitting a Linear Regression model for BMI dataset

--- linear_regression.py
+++ linear_regression.py
@@ -1,3 +1,17 @@
+import pandas as pd
 from sklearn.linear_model import LinearRegression
+
+# Assign the dataframe to this variable.
+# TODO: Load the data
+bmi_life_data = pd.read_csv('bmi_and_life_expectancy.csv')
+print(bmi_life_data)
+
+# Make and fit the linear regression model
+#TODO: Fit the model and Assign it to bmi_life_model
 model = LinearRegression()
-model.fit(x_values, y_values)
+bmi_life_model = model.fit(bmi_life_data[['BMI']], bmi_life_data[['Life expectancy']])
+
+# Mak a prediction using the model
+# TODO: Predict life expectancy for a BMI value of 21.07931
+laos_life_exp = bmi_life_model.predict(21.07931)
+print(laos_life_exp)
